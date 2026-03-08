d8913e61150a874b50b2d85bfd8abbc9a8e29a3c
MinhajNp <minhajmohammednp@gmail.com>
2026-03-08 17:47:23 +0530

    feat: implement OTP entity and generator utility

---
+++ src/modules/auth/otp.entity.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+export interface IOtp extends Document {
+  email: string
+  otp: string
+  expiresAt: Date
+}
+
+const otpSchema = new Schema<IOtp>({
+  email: {
+    type: String,
+    required: true
+  },
+
+  otp: {
+    type: String,
+    required: true
+  },
+
+  expiresAt: {
+    type: Date,
+    required: true
+  }
+
+})
+
+otpSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 })
