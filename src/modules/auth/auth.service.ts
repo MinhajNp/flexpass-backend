2201d89bac4e6be13d486aef9f2a7a3e884bafd6
MinhajNp <minhajmohammednp@gmail.com>
2026-03-08 15:37:57 +0530

    feat: implement auth service with user registration logic

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,1 +1,34 @@
-export class AuthService {}
+import bcrypt from "bcrypt"
+import { UserRepository } from "../user/user.repository"
+import { IUser } from "../user/user.entity"
+import { AppError } from "../../utils/AppError"
+import { Role } from "../../enums/role.enum"
+
+export class AuthService {
+
+  private userRepository = new UserRepository()
+
+  async register(data: {
+    name: string
+    email: string
+    password: string
+    role: Role
+  }): Promise<IUser> {
+
+    const existingUser = await this.userRepository.findByEmail(data.email)
+
+    if (existingUser) {
+      throw new AppError("User already exists", 400)
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10)
+
+    const newUser = await this.userRepository.createUser({
+      ...data,
+      password: hashedPassword
+    })
+
+    return newUser
+  }
+
+}
