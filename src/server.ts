a22ce8d1672b4350473aba466768109bf77e140a
MinhajNp <minhajmohammednp@gmail.com>
2026-09-09 10:26:29 +0530

    chore: configure code quality and environment

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
-import app from "./app.js";
+import app from './app.js';
+import { env } from './config/env.js';
 
-const PORT = 3000;
-
-app.listen(PORT, ()=>{
-    console.log(`server started on port ${PORT}`)
+app.listen(env.PORT, () => {
+  console.log(`server started on port ${env.PORT}`);
 });
